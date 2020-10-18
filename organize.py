45b83a01a23a2a11ab5413571293eb015af4a8ee
michael <connellmp@gmail.com>
2020-10-18 10:19:21 -0700

    update error messages

--- organize.py
+++ organize.py
@@ -46,7 +46,7 @@ def organize_files(drive):
     elif operating_system == 'Windows':  # Windows
         drive = drive + '\\'
         if not os.path.exists(drive):
-            print("ERROR! Invalid location")
+            print(f"ERROR! {drive} is not a valid location")
             return
         files = os.listdir(drive)
         extns = {os.path.splitext(file)[1].strip('.') for file in files}
